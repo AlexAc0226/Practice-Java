8e0cf3b7b382e1087c05a1a5c57c75740d294967
Alexis Correa <alexis.correa026@gmail.com>
2022-10-14 16:37:27 -0300

    Hola mundo desde consola, Java

---
+++ proyect1/src/com/company/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class HelloWorld {
+    public static void main(String[] arg){
+        int n1 = 5, n3 = 5;
+        int n2 = 4;
+        int result = n1 + n2 + n3;
+
+        System.out.print("El resultado es: " + result);
+    }
+}
