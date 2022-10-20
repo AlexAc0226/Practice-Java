3795a93b50abe3148ea979320586cd832e65b1ce
Alexis Correa <alexis.correa026@gmail.com>
2022-10-19 23:53:30 -0300

    Métodos de la clase String

---
+++ StringClass/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String cadena = " Hola Alex";
+
+        System.out.println(cadena.length());
+        System.out.println(cadena.trim());
+
+        String cadena2 = "Holaa ALEXNADER";
+        if (cadena.equals(cadena2)) System.out.println("Son iguales");
+        else System.out.println("Son diferentes");
+            
+
+
+
+
+
+
+    }
+}
